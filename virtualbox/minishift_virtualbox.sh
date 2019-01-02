49d8cdfbe88313d20750b91c0888366d602ea9a0
Lalatendu Mohanty <lmohanty@redhat.com>
2019-01-02 15:54:35 +0530

    Updated script after initial testing
    The code for restarting networking is removed as it disconnects the VPN
    
    Signed-off-by: Lalatendu Mohanty <lmohanty@redhat.com>

--- virtualbox/minishift_virtualbox.sh
+++ virtualbox/minishift_virtualbox.sh
@@ -57,28 +57,30 @@ cluster_create()
     VBoxManage createvm --name master --ostype Fedora_64 --register 
     VBoxManage modifyvm master --cpus 2 --memory 8590 --vram 16
     VBoxManage modifyvm master --nic1 hostonly
+    VBoxManage modifyvm master --nictype1 virtio
     VBoxManage modifyvm master --hostonlyadapter1 vboxnet0
     VBoxManage modifyvm master --nic2 nat
-    VBoxManage modifyvm master --macaddress1 2e393f5e0176 
+    VBoxManage modifyvm master --nictype2 virtio
+    VBoxManage modifyvm master --macaddress1 3aceb1219fb2
     VBoxManage storagectl master --name "SATA Controller" --add sata --bootable on --portcount 1 
     VBoxManage storageattach master  --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium ~/VirtualBox\ VMs/master/test1-master-0.vmdk
     
     # Worker configuration
     VBoxManage createvm --name worker --ostype Fedora_64 --register
-    VBoxManage modifyvm worker --cpus 2 --memory 2096 --vram 16
+    VBoxManage modifyvm worker --cpus 2 --memory 3096 --vram 16
     VBoxManage modifyvm worker --nic1 hostonly
+    VBoxManage modifyvm worker --nictype1 virtio
     VBoxManage modifyvm worker --hostonlyadapter1 vboxnet0
     VBoxManage modifyvm worker --nic2 nat
-    VBoxManage modifyvm worker --macaddress1 06dc5cfa5497
+    VBoxManage modifyvm worker --nictype1 virtio
+    VBoxManage modifyvm worker --macaddress1 080027c72a33
     VBoxManage storagectl worker --name "SATA Controller" --add sata --bootable on --portcount 1
     VBoxManage storageattach worker  --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium ~/VirtualBox\ VMs/worker/test1-worker-0-98nsr.vmdk
     
     hostonlyip=$(VBoxManage list hostonlyifs | grep IPAddress | head -1 | awk '{print $2}')
-    
-    # Coredns set forward for domain
-    nameserver=$(nslookup google.com | head -1 | awk '{print $2}')
-    sed -i '' "s/mynameserver/$nameserver/g" Corefile
-
+   
+    update_nameserver
+ 
     echo "Adding tt.testing to /etc/resolver, It will ask for sudo password"
     # Add nameserver to resolv.conf which VM actually take
     echo "search tt.testing" | sudo tee /etc/resolv.conf
@@ -87,6 +89,23 @@ cluster_create()
     # Coredns ip add to resolver (tt.testing)
     sudo mkdir -p /etc/resolver
     echo "nameserver $hostonlyip" | sudo tee /etc/resolver/tt.testing
+}
+
+update_nameserver()
+{ 
+    saveIFS="$IFS"
+    IFS=$'\n'
+    nameservers=$(scutil --dns | sed -n '/^resolver #1/,/^resolver #2/p;/^resolver #2/q' | grep nameserver | awk '{print $3}')
+    IFS="$saveIFS"
+    for i in "${nameservers[@]}"; do
+       nslookup google.com $i
+       if [ $? -eq 0 ]
+       then
+           i=$(echo $i | tr -d '\r')
+           sed -i '' "s/mynameserver/$i/g" Corefile
+           break
+       fi
+    done
 }
 
 cluster_start()
@@ -109,8 +128,7 @@ modify_corefile()
           echo $line
           IFS=' ' read -a array <<< $line
           masterIp=$(echo "${array[1]}"|tr "(" " "|tr ")" " ")
-        
-          if [ "2e:39:3f:5e:1:76" = "${array[3]}" ]; then
+          if [ "3a:ce:b1:21:9f:b2" = "${array[3]}" ]; then
             echo "Master IP address is $masterIp"
             break 2
           fi
@@ -123,8 +141,7 @@ modify_corefile()
           echo $line
           IFS=' ' read -a array <<< $line
           workerIp=$(echo "${array[1]}"|tr "(" " "|tr ")" " ")
-        
-          if [ "6:dc:5c:fa:54:97" = "${array[3]}" ]; then
+          if [ "8:0:27:c7:2a:33" = "${array[3]}" ]; then
             echo "Worker IP address is $workerIp"
             break 2
           fi
@@ -156,28 +173,12 @@ check_cluster()
     fi
 }
 
-restart_network()
-{
-   echo "Restarting Network Setup ..."
-   saveIFS="$IFS"
-   IFS=$'\n'
-   services=( $(networksetup -listallnetworkservices | sed '/An asterisk ([*]) denotes that a network service is disabled./d; s/^[*]//') )
-   IFS="$saveIFS"
-
-   for i in "${services[@]}"; do
-        # This sleep is required to make sure we provide enough time to settle the network
-        sleep 2
-	networksetup -setnetworkserviceenabled "$i" off && networksetup -setnetworkserviceenabled "$i" on
-   done
-}
-
 cluster_delete()
 {
     check_cluster
     cluster_stop
     VBoxManage unregistervm master --delete
     VBoxManage unregistervm worker --delete
-    restart_network
     coredns_stop
     if [ -f Corefile_bak ]; then
 	rm Corefile && mv Corefile_bak Corefile
@@ -200,12 +201,6 @@ coredns_stop()
     pkill coredns
 }
 
-
-if [ "$#" -ne 1 ]; then
-    usage
-    exit 0
-fi
-
 usage()
 {
     usage="$(basename "$0") [[create | start | stop | delete] | [-h]]
@@ -221,22 +216,32 @@ where:
 
 }
 
-while [ "$1" != "" ]; do
-    case $1 in
-        create )           prerequisites
-                           cluster_create
-                           ;;
-        start )            cluster_start
-                           ;;
-        stop )             cluster_stop
-                           ;;
-        delete )           cluster_delete
-                           ;;
-        -h | --help )      usage
-                           exit
-                           ;;
-        * )                usage
-                           exit 1
-    esac
-    shift
-done
+main()
+{
+    if [ "$#" -ne 1 ]; then
+        usage
+        exit 0
+    fi
+    
+    while [ "$1" != "" ]; do
+        case $1 in
+            create )           prerequisites
+                               cluster_create
+                               ;;
+            start )            cluster_start
+                               ;;
+            stop )             cluster_stop
+                               ;;
+            delete )           cluster_delete
+                               ;;
+            -h | --help )      usage
+                               exit
+                               ;;
+            * )                usage
+                               exit 1
+        esac
+        shift
+    done
+}
+
+main "$@"; exit
